31ca1828bd38b50a4977ddc73f157194fdea6e38
Aaren Ho <aarenho11@gmail.com>
2026-05-09 16:41:57 +0900

    dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function DashboardPage() {
+  return (
+    <main className="p-12 space-y-20">
+      <h1 className="text-3xl font-bold">Suitely Dashboard</h1>
+
+      <div className="border rounded-lg p-6">
+        <input type="file" accept="pdf"/>
+      </div>
+    </main>
+  );
+}
